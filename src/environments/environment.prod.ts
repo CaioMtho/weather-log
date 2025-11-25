9f3e2d97f3436d645b171dff2a086680b0ce3977
CaioMtho <caiomathol@gmail.com>
2025-11-24 23:18:08 -0300

    chore: environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBarUxa26oz1n15Gxw73p2JtF9p-CcK3Hc",
+    authDomain: "weather-log-mqtt.firebaseapp.com",
+    databaseURL: "https://weather-log-mqtt-default-rtdb.firebaseio.com",
+    projectId: "weather-log-mqtt",
+    storageBucket: "weather-log-mqtt.firebasestorage.app",
+    messagingSenderId: "729506692138",
+    appId: "1:729506692138:web:bb6238a5d128940ba29753",
+    measurementId: "G-NQL6Y1FWZR"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,19 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// src/environments/environment.ts
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBarUxa26oz1n15Gxw73p2JtF9p-CcK3Hc",
+    authDomain: "weather-log-mqtt.firebaseapp.com",
+    databaseURL: "https://weather-log-mqtt-default-rtdb.firebaseio.com",
+    projectId: "weather-log-mqtt",
+    storageBucket: "weather-log-mqtt.firebasestorage.app",
+    messagingSenderId: "729506692138",
+    appId: "1:729506692138:web:bb6238a5d128940ba29753",
+    measurementId: "G-NQL6Y1FWZR"
+  }
 };
 
 /*
